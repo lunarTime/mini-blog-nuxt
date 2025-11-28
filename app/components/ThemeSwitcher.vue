e84dd45499cc5c3df3524c24429dd6e04fee94a6
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-28 23:42:58 +0500

    feat: add theme switcher component

---
+++ app/components/ThemeSwitcher.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    const colorMode = useColorMode()
+
+    const toggleTheme = () => {
+        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+</script>
+
+<template>
+    <UButton
+        size="md"
+        variant="soft"
+        color="primary"
+        @click.prevent.stop="toggleTheme"
+        type="button"
+        to="#"
+        :icon="colorMode.value === 'dark' ? 'i-heroicons-moon' : 'i-heroicons-sun'"
+    />
+</template>
