b51007bbca4c4c76811e1ca5f7ebf593dd180b04
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-28 22:09:58 +0500

    feat: add utility function to format dates in YYYY-MM-DD format

---
+++ app/utils/formatDate.ts
@@ -0,0 +1,9 @@
+export function formatDate(): string {
+    const now = new Date()
+
+    return [
+        now.getFullYear(),
+        String(now.getMonth() + 1).padStart(2, '0'),
+        String(now.getDate()).padStart(2, '0')
+    ].join('-')
+}
