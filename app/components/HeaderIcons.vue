90f63755e0ff201f74e88729bc3e847bbba0e8e8
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-29 00:40:24 +0500

    feat: add component to display social networks and a theme switcher

---
+++ app/components/HeaderIcons.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <UTooltip text="Open this project on GitHub">
+            <UButton
+                color="neutral"
+                variant="ghost"
+                to="https://github.com/lunarTime/mini-blog-nuxt"
+                target="_blank"
+                icon="i-simple-icons-github"
+                aria-label="GitHub"
+            />
+        </UTooltip>
+        <UTooltip text="My LinkedIn">
+            <UButton
+                color="neutral"
+                variant="ghost"
+                to="https://www.linkedin.com/in/nikita-eremeev-556238283/"
+                target="_blank"
+                icon="i-simple-icons-logmein"
+                aria-label="LinkedIn"
+            />
+        </UTooltip>
+        <ThemeSwitcher />
+    </div>
+</template>
