4a1f64743f5da16b8a41277c74466a094b7e6514
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-28 22:56:46 +0500

    feat: add custom 404 error page

---
+++ app/error.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+    import type { NuxtError } from '#app'
+
+    defineProps<{
+        error: NuxtError
+    }>()
+
+    useSeoMeta({
+        title: '404 - Page not found | Nuxt 4 mini-blog',
+        description: 'The page you are looking for does not exist.'
+    })
+</script>
+
+<template>
+    <UError
+        redirect="/blog"
+        :clear="{
+            size: 'xl',
+            icon: 'i-lucide-arrow-left',
+            variant: 'soft',
+            label: 'Back to blog'
+        }"
+        :error="error"
+    />
+</template>
