5f01c0c005001224d8993f86461a6b3e1b7d106f
Eremeev Nikita <ena2000.11@gmail.com>
2025-11-28 23:42:16 +0500

    feat: update header component with slideover mode and add theme switcher

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,21 @@
 <template>
     <UApp>
-        <NuxtLayout>
-            <UHeader class="mb-10">
-                <template #title>
-                    <div class="flex items-center gap-2">
-                        <i class="mdi mdi-post text-2xl"></i>
-                        <span>Nuxt 4 blog</span>
-                    </div>
-                </template>
-            </UHeader>
+        <UHeader
+            class="mb-10"
+            mode="slideover"
+        >
+            <template #title>
+                <div class="flex items-center justify-between gap-2 w-full">
+                    <i class="mdi mdi-post text-2xl"></i>
+                    <span>Nuxt 4 blog</span>
+                </div>
+            </template>
+            <template #right>
+                <ThemeSwitcher />
+            </template>
+        </UHeader>
 
+        <NuxtLayout>
             <NuxtPage />
         </NuxtLayout>
     </UApp>
